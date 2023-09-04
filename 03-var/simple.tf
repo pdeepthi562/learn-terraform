ede4057dccf2ccd8045e2a53e2cf5900f3ee7d9e
dpathim <dpdevops6@gmail.com>
2023-09-04 17:36:28 -0400

    Terraform

--- 03-var/simple.tf
+++ 03-var/simple.tf
@@ -5,3 +5,35 @@ variable "fruit_name"{
 output "fruit_name" {
   value = var.fruit_name
 }
+
+# list Variable
+variable "fruit" {
+  default = [
+    "apple",
+    "banana"
+]
+  //default =[ "apple","banana" ] //single line syntax
+
+}
+
+# map variable ,plain
+variable "fruit_stock" {
+  default = {
+    apple = 100
+    banana = 200
+
+  }
+}
+# map variable map of maps
+variable "fruit_stock_with_price" {
+  default = {
+    apple = {
+      stock = 100
+      price = 3
+    }
+    banana = {
+      stock = 400
+      price = 1
+    }
+  }
+}
