c7d0fdc90b38d2f8a034d9f3ff80ca8229717407
dpathim <dpdevops6@gmail.com>
2023-09-05 19:09:55 -0400

    Terraform

--- 07-module/local_module/data.tf
+++ 07-module/local_module/data.tf
@@ -1,10 +1,10 @@
-data "aws_ami" "ami" {
+data "aws_ami_ids" "ami" {
   name_regex  = "Centos-*"
   owners      = ["973714476881"]
 
 }
 //data in the local module will only copy data in the main module though we give the output.but when we give the output in the main module it will prite the output.
 output "ami" {
-  value = data.aws_ami.ami
+  value = data.aws_ami_ids.ami
 
 }
