bb1311af83a80f428434224614b03ce12119ed4d
dpathim <dpdevops6@gmail.com>
2023-09-05 14:44:23 -0400

    Terraform

---
+++ 06-loops/count/main.tf
@@ -0,0 +1,15 @@
+variable "components" {
+  default = ["frontend","mongodb"]
+}
+
+resource "aws_instance" "instance" {
+  count = length(var.components)
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-038da0861b4c1788f" ]
+  tags = {
+    Name = ""
+    //Name = var.components[count.index]
+    //Name = element(var.components, count.index)
+  }
+}
