4bace337b80e387678150cdead73ab5f07c50b63
dpathim <dpdevops6@gmail.com>
2023-09-06 16:01:43 -0400

    Terraform

--- 07-module/local_module/data.tf
+++ 07-module/local_module/data.tf
@@ -1,11 +1,25 @@
 # To know the ami ids of the centos-*
 
-data "aws_ami_ids" "ami" {
-  name_regex  = "Centos-*"
-  owners      = ["973714476881"]
-
-}
+#data "aws_ami_ids" "ami" {
+#  name_regex  = "Centos-*"
+#  owners      = ["973714476881"]
+#
+#}
 //data in the local module will only copy data in the main module though we give the output.but when we give the output in the main module it will prite the output.
+#output "ami" {
+#  value = data.aws_ami_ids.ami
+#}
+
+
+# using one amiid from the above and creating the resource
+
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
+  owners = ["973714476881"]
+}
 output "ami" {
-  value = data.aws_ami_ids.ami
+  value = data.aws_ami.ami.id
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-038da0861b4c1788f" ]
 }
