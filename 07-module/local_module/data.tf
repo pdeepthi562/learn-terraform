c8fd122e2330695bc7024ea41f284be4b8a0781a
dpathim <dpdevops6@gmail.com>
2023-09-06 15:44:59 -0400

    Terraform

--- 07-module/local_module/data.tf
+++ 07-module/local_module/data.tf
@@ -1,3 +1,5 @@
+# To know the ami ids of the centos-*
+
 data "aws_ami_ids" "ami" {
   name_regex  = "Centos-*"
   owners      = ["973714476881"]
@@ -6,5 +8,4 @@ data "aws_ami_ids" "ami" {
 //data in the local module will only copy data in the main module though we give the output.but when we give the output in the main module it will prite the output.
 output "ami" {
   value = data.aws_ami_ids.ami
-
 }
