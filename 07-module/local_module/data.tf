7980ccfd3e63758d7542c4d7f43adbe52a1ee082
dpathim <dpdevops6@gmail.com>
2023-09-06 16:09:22 -0400

    Terraform

--- 07-module/local_module/data.tf
+++ 07-module/local_module/data.tf
@@ -18,8 +18,12 @@ data "aws_ami" "ami" {
   name_regex = "Centos-8-DevOps-Practice"
   owners = ["973714476881"]
 }
+
 output "ami" {
-  value = data.aws_ami.ami.id
+  value = data.aws_ami.ami
+}
+resource "aws_instance" "instance" {
+  ami = data.aws_ami.ami.id
   instance_type = "t3.small"
   vpc_security_group_ids = [ "sg-038da0861b4c1788f" ]
 }
