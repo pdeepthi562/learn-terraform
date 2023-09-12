0794aa57a4dd53f6bfd17ae29eaf930814957056
dpathim <dpdevops6@gmail.com>
2023-09-12 15:21:58 -0400

    Terraform

--- vpc-in-basic-way/main.tf
+++ vpc-in-basic-way/main.tf
@@ -10,7 +10,7 @@ variable "app_subnets" {
   default = ["10.0.2.0/24", "10.0.3.0/24"]
 }
 
-variable "bd_subnets" {
+variable "db_subnets" {
   default = ["10.0.4.0/24", "10.0.5.0/24"]
 
 }
@@ -25,5 +25,24 @@ resource "aws_subnet" "public" {
   cidr_block = var.public_subnets[count.index]
   tags = {
     Name ="public-${count.index+1}"
+  }
+}
+
+resource "aws_subnet" "app" {
+  count =length(var.app_subnets)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.app_subnets[count.index]
+  tags = {
+    Name ="app-${count.index+1}"
+  }
+}
+
+
+resource "aws_subnet" "db" {
+  count =length(var.db_subnets)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.db_subnets[count.index]
+  tags = {
+    Name ="db-${count.index+1}"
   }
 }
