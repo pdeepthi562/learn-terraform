0c5c0127a1cce5106b11eea9f9c866cf321f45b4
dpathim <dpdevops6@gmail.com>
2023-09-12 15:13:25 -0400

    Terraform

--- vpc-in-basic-way/main.tf
+++ vpc-in-basic-way/main.tf
@@ -22,7 +22,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   count =length(var.public_subnets)
   vpc_id = aws_vpc.main.id
-  cidr_block = "var.public_subnets[count.index]"
+  cidr_block = var.public_subnets[count.index]
   tags = {
     Name ="public-${count.index+1}"
   }
